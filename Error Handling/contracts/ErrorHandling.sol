bd4e19be1f4f201f654b966db0ce36df4259f7d6
Ajith <58944893+ajith-m-doodlebug@users.noreply.github.com>
2022-03-11 03:22:33 +0530

    Add files via upload

---
+++ Error Handling/contracts/ErrorHandling.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract ErrorHandling{
+
+    uint public numberOne = 15;
+    uint public numberTwo = 1;
+
+    function errorRequire() public view  {
+        require(numberOne > numberTwo, "The number one should be greater than number two.");
+    }
+
+    function errorRevert() public view  {
+        if(numberOne > numberTwo){
+            revert("The number one should be greater than number two.");
+        }
+    }
+
+    function errorAssert() public view {
+        assert(numberOne == 15);
+    }
+
+}
